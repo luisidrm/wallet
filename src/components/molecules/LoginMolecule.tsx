615dc9b63f81c80dc7ade4783d89521a33002a40
luisidrm <riosluisdaniel195@gmail.com>
2025-10-05 20:35:00 -0400

    Fix: sustitution of navigation for prof

--- src/components/molecules/LoginMolecule.tsx
+++ src/components/molecules/LoginMolecule.tsx
@@ -1,34 +1,36 @@
+import { useNavigate } from "react-router"
 import TextInput from "../atoms/TextInput";
 import PasswordInput from "../atoms/PasswordInput";
 import { Button } from "../ui/button";
 import BigCard from "../organisms/BigCard";
 
-type Props ={
-  user:string
-  password:string
-  setUser:(arg:string)=>void
-  setPassword:(arg:string)=>void
+type Props = {
+  user: string
+  password: string
+  setUser: (arg: string) => void
+  setPassword: (arg: string) => void
 }
 
-export default function LoginMolecule({user,password,setUser, setPassword}:Props){
-
-  const goToDashboard=()=>{
-    window.location.href="/dashboard"
+export default function LoginMolecule({ user, password, setUser, setPassword }: Props) {
+  const navigate = useNavigate()
+  const goToDashboard = () => {
+    navigate("/dashboard")
   }
 
-  return(
+  return (
     <div className="flex flex-col gap-2 justify-center place-items-center">
-      <BigCard 
+      <BigCard
         account={{
-          id:"1",
-          cardName:"My Wallet",
-          cardNumber:"00000000000000",
-        balance:0,
-      currency:"USD"}}
+          id: "1",
+          cardName: "My Wallet",
+          cardNumber: "00000000000000",
+          balance: 0,
+          currency: "USD"
+        }}
       />
       <TextInput placeholder="Nombre de Usuario" value={user} changeFn={setUser} />
-      <PasswordInput placeholder="Contraseña" value={password} changeFn={setPassword}/>
-      <Button onClick={()=>goToDashboard()} type="button" className="w-full mt-2">Login</Button>
+      <PasswordInput placeholder="Contraseña" value={password} changeFn={setPassword} />
+      <Button onClick={() => goToDashboard()} type="button" className="w-full mt-2">Login</Button>
     </div>
   )
 }
